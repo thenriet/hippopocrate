735722e9218518ad6490afea4dc711ac7b8f3039
thenriet <dabalx@gmail.com>
2023-02-20 16:35:05 +0100

    added Angular

---
+++ client/src/main/java/co/simplon/hippopocrate/HippopocrateApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.hippopocrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippopocrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HippopocrateApplication.class, args);
+	}
+
+}
---
+++ client/src/test/java/co/simplon/hippopocrate/HippopocrateApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.hippopocrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippopocrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
