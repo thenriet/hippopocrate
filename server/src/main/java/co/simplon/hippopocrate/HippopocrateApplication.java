53702b5f0a1fc2478ab3aee18d5f3bb5eb1a9a00
thenriet <dabalx@gmail.com>
2023-02-21 16:29:07 +0100

    controller for patients created and able to connect to the db

--- server/src/main/java/co/simplon/hippopocrate/HippopocrateApplication.java
+++ server/src/main/java/co/simplon/hippopocrate/HippopocrateApplication.java
@@ -27,11 +27,11 @@ public class HippopocrateApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		String strDate = "1993-09-14";
-		Date date = Date.valueOf((String) strDate);
-		Patient patient = new Patient("Amine", "Bouhaouche", date, 1, "Rue de la résistance");
-		Patient patient1 = new Patient("Ondine", "Debienne", date, 1, "Rue de la résistance");
-		System.out.println(psi.fetchPatientList());
+//		String strDate = "1993-09-14";
+//		Date date = Date.valueOf((String) strDate);
+//		Patient patient = new Patient("Amine", "Bouhaouche", date, 1, "Rue de la résistance");
+//		Patient patient1 = new Patient("Ondine", "Debienne", date, 1, "Rue de la résistance");
+//		System.out.println(psi.fetchPatientList());
 //		psi.fetchPatientList().toString();
 //		psi.updatePatient(patient1, 2);
 
--- server/src/main/java/co/simplon/hippopocrate/controller/PatientController.java
+++ server/src/main/java/co/simplon/hippopocrate/controller/PatientController.java
@@ -4,17 +4,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.hippopocrate.model.Patient;
 import co.simplon.hippopocrate.service.PatientServiceImpl;
 
-
-@Controller 
+@CrossOrigin(origins = "http://localhost:3000")
+@Controller
+@RestController
+@RequestMapping("api/")
 public class PatientController {
 	
 	@Autowired
 	private  PatientServiceImpl psi;
 	
+    @GetMapping("patients")
 	public List<Patient> fetchPatientList() {
 		return psi.fetchPatientList();
 	}
