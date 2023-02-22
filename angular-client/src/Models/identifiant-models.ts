4392c9bb29ae72b123b39be65669c3a77d109dcc
Caroline Hilmoine <caro52130@gmail.com>
2023-02-22 11:17:12 +0100

    Page identification

---
+++ angular-client/src/Models/identifiant-models.ts
@@ -0,0 +1,4 @@
+export class identifiantModel {
+    id!:Number; 
+    password!:string;
+}
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageConnexionComponent } from './page-connexion/page-connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageConnexionComponent
   ],
   imports: [
     BrowserModule,
